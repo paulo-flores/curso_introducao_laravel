9c318d102381f927c17fe1fa3ba46016dacfdd48
paulo-flores <paulorgs.flores@gmail.com>
2019-09-15 17:26:32 -0300

    Primeiro commit do projeto

---
+++ app/Http/Controllers/ContatoController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class ContatoController extends Controller
+{
+    //
+
+    public function index()
+    {
+        return "Esse é o index do ContatoController";
+    }
+
+}
